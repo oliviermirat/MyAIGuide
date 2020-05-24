dc387a46afe099ec5daa63d40059b6aaa2da3a22
Eva Martínez <3v4.mr6@gmail.com>
2020-05-24 17:41:27 +0200

    basic function

---
+++ src/MyAIGuide/moves_participant2.py
@@ -0,0 +1,20 @@
+import glob
+import pandas as pd
+from pathlib import Path
+
+
+def retrieve_moves_participant2(summary=False):
+    """ Function creates a Dataframe of Moves data for Participant 2
+        concatenates all csv files in a pandas DataFrame
+    """
+    path = Path('../data/raw/ParticipantData/Participant2Anonymized/MovesAppData/daily')
+
+    if(not summary):
+        folder_name = 'activities'
+    else:
+        folder_name = 'summary'
+
+    all_files = glob.glob(os.path.join(path, folder_name, "*.csv"))
+    df = pd.concat((pd.read_csv(f) for f in all_files))
+
+    return df
