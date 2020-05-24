a8551e45e262ac2302e0ebedcc52b854a985aa87
Eva Martínez <3v4.mr6@gmail.com>
2020-05-24 21:37:15 +0200

    fix path to daily summary

--- src/MyAIGuide/moves_participant2.py
+++ src/MyAIGuide/moves_participant2.py
@@ -4,18 +4,16 @@ import pandas as pd
 from pathlib import Path
 
 
-def retrieve_moves_participant2(summary=False):
+def retrieve_moves_participant2():
     """ Function creates a Dataframe of Moves data for Participant 2
         concatenates all csv files in a pandas DataFrame
     """
-    path = Path('../data/raw/ParticipantData/Participant2Anonymized/MovesAppData/daily')
+    path = Path('../data/raw/ParticipantData/Participant2Anonymized/MovesAppData/daily/summary')
 
-    if(not summary):
-        folder_name = 'activities'
-    else:
-        folder_name = 'summary'
-
-    all_files = glob.glob(os.path.join(path, folder_name, "*.csv"))
+    all_files = glob.glob(os.path.join(path, "*.csv"))
     df = pd.concat((pd.read_csv(f) for f in all_files))
+
+    # TODO:
+    # cleanup of df to look like https://github.com/oliviermirat/MyAIGuide/blob/master/scripts/1_createDataFrame.py
 
     return df
