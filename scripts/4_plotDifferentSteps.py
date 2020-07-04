6e0c88462e323b2b4b0d20463238db0f87dfec64
Eva Martínez <3v4.mr6@gmail.com>
2020-07-04 18:50:05 +0200

    refactor code independent of dataframe creation

--- scripts/4_plotDifferentSteps.py
+++ scripts/4_plotDifferentSteps.py
@@ -8,9 +8,9 @@ import pandas as pd
 import sys
 
 from dataFrameUtilities import (
-    addDataToTrackerMeanSteps,
+    insert_data_to_tracker_mean_steps,
     addInsultIntensityColumns,
-    adjustVarAndPlaceInData,
+    adjust_var_and_place_in_data,
     getInsultAboveThreshold,
     getPainAboveThreshold,
     selectColumns,
@@ -41,22 +41,22 @@ period5 = selectTime(data, "2017-10-17", "2018-07-02")  # fitbit and moves
 period6 = selectTime(data, "2018-07-03", "2018-07-30")  # fitbit and moves and googlefit
 period7 = selectTime(data, "2018-07-31", "2020-02-01")  # fitbit and googlefit
 
-# Creating the trackerMeanSteps variable in the dataframe and puts harmonized steps value inside
-data["trackerMeanSteps"] = data["steps"]
-data = adjustVarAndPlaceInData(period1, data, "googlefitSteps", "basisPeakSteps", "2015-12-26", "2016-06-21")
-data = addDataToTrackerMeanSteps(period2, data, "basisPeakSteps", "2016-06-22", "2016-09-01")
-data = adjustVarAndPlaceInData(period3, data, "basisPeakSteps", "steps", "2016-09-02", "2017-01-01")
-data = addDataToTrackerMeanSteps(period4, data, "steps", "2017-01-02", "2017-10-16")
-data = adjustVarAndPlaceInData(period5, data, "movesSteps", "steps", "2017-10-17", "2018-07-02")
-data = adjustVarAndPlaceInData(period6, data, "movesSteps", "steps", "2018-07-03", "2018-07-30")
-data = adjustVarAndPlaceInData(period7, data, "googlefitSteps", "steps", "2018-07-31", "2020-02-01")
+# Creating the tracker_mean_steps variable in the dataframe and puts harmonized steps value inside
+data["tracker_mean_steps"] = data["steps"]
+data = adjust_var_and_place_in_data(period1, data, "googlefitSteps", "basisPeakSteps")
+data = insert_data_to_tracker_mean_steps(period2, data, "basisPeakSteps")
+data = adjust_var_and_place_in_data(period3, data, "basisPeakSteps", "steps")
+data = insert_data_to_tracker_mean_steps(period4, data, "steps")
+data = adjust_var_and_place_in_data(period5, data, "movesSteps", "steps")
+data = adjust_var_and_place_in_data(period6, data, "movesSteps", "steps")
+data = adjust_var_and_place_in_data(period7, data, "googlefitSteps", "steps")
 
 # Plotting results
 
-steps = selectColumns(data, ["steps", "movesSteps", "googlefitSteps", "basisPeakSteps", "trackerMeanSteps"])
+steps = selectColumns(data, ["steps", "movesSteps", "googlefitSteps", "basisPeakSteps", "tracker_mean_steps"])
 fig, axes = plt.subplots(nrows=2, ncols=1)
 steps.plot(ax=axes[0])
 axes[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-selectColumns(data, ["trackerMeanSteps"]).plot(ax=axes[1])
+selectColumns(data, ["tracker_mean_steps"]).plot(ax=axes[1])
 axes[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
