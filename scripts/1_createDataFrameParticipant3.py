27910944cf1eccfc8562abbc15181451033b149f
ann-w <anniewong@live.nl>
2020-07-19 12:20:31 +0200

    [issue#44] create df p3

---
+++ scripts/1_createDataFrameParticipant3.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 12:18:43 2020
+
+@author: anniewong
+"""
+
+#%% import libraries
+import pickle
+import numpy as np
+import pandas as pd
+from MyAIGuide.data.google_fit import get_google_fit_steps
+from MyAIGuide.data.storePainIntensitiesForParticipants2to9 import storePainIntensitiesForParticipants2to9
+
+
+#%% Create master dataframe
+dates = pd.date_range("2018-10-22", periods=180, freq="1D") 
+
+columnnames = [
+        "googlefitsteps",
+        "happiness",
+        "kneepain", 
+        'patellartendonthrobbing'
+           ]
+
+data = pd.DataFrame(np.nan, columns = columnnames, index = dates)
+
+#%% Fill dataframe with data
+
+# Directory to participant4 data
+datadir = "../data/raw/ParticipantData/Participant3Anonymized/"
+
+# Store pain intensities 
+new_data= storePainIntensitiesForParticipants2to9(datadir, data)
+
+# Store Google fit steps
+new_data=get_google_fit_steps(datadir, new_data)
+
+# Saving the dataframe in a text
+output = open("../data/preprocessed/preprocessedDataParticipant3.txt", "wb")
+pickle.dump(data, output)
+output.close()
