c04935af64bfcba387e10faad2401dac670e938b
Eva Martínez <3v4.mr6@gmail.com>
2020-08-24 20:15:06 +0200

    take date from file name instead from date column to fix format inconsistencies

--- src/MyAIGuide/data/movesDataGatheredFromWebExport.py
+++ src/MyAIGuide/data/movesDataGatheredFromWebExport.py
@@ -1,5 +1,5 @@
 import csv
-import datetime
+from datetime import datetime
 import os
 import os.path
 import pickle
@@ -8,28 +8,27 @@ import re
 import numpy as np
 import pandas as pd
 
+
 def movesDataGatheredFromWebExport(fname, data):
-  directory = os.fsencode(fname)
-  for file in os.listdir(directory):
-      name = os.fsdecode(file)
-      if name.endswith(".csv"):
-          filename = (fname + name)
-          with open(filename, newline="") as csvfile:
-              spamreader = csv.reader(csvfile)
-              count = 0
-              for row in spamreader:
-                  count = count + 1
-                  if count > 1 and len(row):
-                      dateMoves = re.split("/", row[0])
-                      month = dateMoves[0]
-                      day = dateMoves[1]
-                      year = dateMoves[2]
-                      if int(day) < 10:
-                          day = "0" + str(day)
-                      if int(month) < 10:
-                          month = "0" + str(month)
-                      year = "20" + year
-                      date = year + "-" + month + "-" + day
-                      if row[1] == "walking":
-                          data.loc[date, "movesSteps"] = int(row[5])
+    directory = os.fsencode(fname)
+    for file in os.listdir(directory):
+        name = os.fsdecode(file)
+        if name.endswith(".csv"):
+            filename = (fname + name)
+            date_from_name = re.split(".csv", re.split("_", name)[1])[0]
+            print("date_from_name:", date_from_name)
+            year = date_from_name[0:4]
+            month = date_from_name[4:6]
+            day = date_from_name[6:8]
+            date = datetime.strptime(year + "-" + month + "-" + day, '%Y-%m-%d')
+            with open(filename, newline="") as csvfile:
+                spamreader = csv.reader(csvfile)
+                count = 0
+                for row in spamreader:
+                    count = count + 1
+                    if count > 1 and len(row):
+                        dateMoves = re.split("/", row[0])
+                        if row[1] == "walking":
+                            if row[1] == "walking":
+                                data.loc[date, "movesSteps"] = int(row[5])
   return data
