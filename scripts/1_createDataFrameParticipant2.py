e38773c82a9fa955b812b60f533f28cf1402e908
Eva Martínez <3v4.mr6@gmail.com>
2020-08-30 09:03:05 +0200

    lint + add Orux trace data

--- scripts/1_createDataFrameParticipant2.py
+++ scripts/1_createDataFrameParticipant2.py
@@ -4,7 +4,7 @@ Created on Mon Aug 10 2020
 @author: evadatinez
 """
 
-#%% import libraries
+# %% import libraries
 import pickle
 import numpy as np
 import pandas as pd
@@ -12,29 +12,31 @@ from MyAIGuide.data.google_fit import get_google_fit_steps
 from MyAIGuide.data.movesDataGatheredFromWebExport import movesDataGatheredFromWebExport
 from MyAIGuide.data.storePainIntensitiesForParticipants2to9 import storePainIntensitiesForParticipants2to9
 from MyAIGuide.data.calculateCumulatedElevationGainMoves import retrieve_stored_CEG_moves
+from MyAIGuide.data.OruxTrace import OruxTrace
 
 
-#%% Create master dataframe
-dates = pd.date_range("2018-01-31", periods=852, freq="1D") 
+# %% Create master dataframe
+dates = pd.date_range("2018-01-31", periods=852, freq="1D")
 
 columnnames = [
         "googlefitSteps",
         "movesSteps",
         "happiness",
-        "kneepain", 
+        "kneepain",
         "cum_gain_cycling",
-        "cum_gain_walking"
+        "cum_gain_walking",
+        "oruxCumulatedElevationGain"
            ]
 
-data = pd.DataFrame(np.nan, columns = columnnames, index = dates)
+data = pd.DataFrame(np.nan, columns=columnnames, index=dates)
 
-#%% Fill dataframe with data
+# %% Fill dataframe with data
 
 # Directory to participant2 data
 datadir = "../data/raw/ParticipantData/Participant2Anonymized/"
 
 # Store Google fit steps
-new_data=get_google_fit_steps(datadir, data)
+data = get_google_fit_steps(datadir, data)
 
 # Storing moves data in dataframe
 fname = "../data/raw/ParticipantData/Participant2Anonymized/MovesAppData/daily/summary/"
@@ -44,8 +46,12 @@ data = movesDataGatheredFromWebExport(fname, data)
 fname = "../data/raw/ParticipantData/Participant2Anonymized/cum_gains_moves_participant2.csv"
 data = retrieve_stored_CEG_moves(fname, data)
 
-# Store pain intensities 
-new_data= storePainIntensitiesForParticipants2to9(datadir, new_data)
+# Retrieving OruxTrace elevation gain data
+fname = "../data/external/Participant2/OruxTrace/"
+data = OruxTrace(data, fname)
+
+# Store pain intensities
+new_data = storePainIntensitiesForParticipants2to9(datadir, data)
 
 # all columns to lowercase
 new_data.columns = [col.lower() for col in new_data.columns]
