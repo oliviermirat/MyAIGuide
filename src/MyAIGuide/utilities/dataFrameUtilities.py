1877e5757fb2fbed1864eb5f1d7e3e2a2b799e43
Eva Martínez <3v4.mr6@gmail.com>
2020-10-02 16:51:21 +0200

    first functional version of the insert_relative_values_columns function

--- src/MyAIGuide/utilities/dataFrameUtilities.py
+++ src/MyAIGuide/utilities/dataFrameUtilities.py
@@ -45,6 +45,29 @@ def insert_rolling_mean_columns(data, column_list, window):
     return data
 
 
+def insert_relative_values_columns(data, column_list, short_window, long_window):
+    """This function selects the columns of a dataframe
+    according to a provided list of strings and inserts a new 
+    column in the dataframe with the ratio of the rolling_sum wrt
+    a short window divided by the rolling_sum wrt a longer window.
+
+    Params:
+        data: original dataframe
+        column_list: list of columns to select
+        short_window: window length to calculate rolling sum for the nummerator
+        long_window: window length to calculate rolling sum for the denominator
+
+    """
+
+    for var in column_list:
+        var_short_window_rsum = data[var].rolling(short_window).sum()
+        var_long_window_rsum = data[var].rolling(long_window).sum()
+        relative_var_name = var + "_relative_" + str(short_window) + "_" + str(long_window)
+        data[relative_var_name] = var_short_window_rsum / var_long_window_rsum
+
+    return data
+
+
 def addInsultIntensityColumns(data, columnList, window, windowMaxInsultDiff):
     scaler = MinMaxScaler()
     data[columnList] = scaler.fit_transform(data[columnList])
