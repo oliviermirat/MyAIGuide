8d71a23d7cf8625bce2c47acd28c9e10dc8715c9
oliviermirat <miratoolivier@gmail.com>
2026-02-28 13:54:58 +0800

    adding script to load the old dataset and run the feb2023 analysis

---
+++ 2026_work/1b_rerun_feb2023_analysis_onOldData_justGenerated.py
@@ -0,0 +1,27 @@
+import pickle
+import sys
+sys.path.insert(1, '../february2023ResearchGatePaperScripts')
+import peaksAnalysis_launch
+
+# Reloading data
+input = open("../data/preprocessed/preprocessedMostImportantDataParticipant1.txt", "rb")
+data = pickle.load(input)
+input.close()
+
+# Analysis parameters
+# 'minMaxTimeToleranceMinus' and 'minMaxTimeTolerancePlus' MUST both be set to 0 to generate the zoomed graphs for the 12_strainBuildUpFastRiseAndTriggersGraph.py graph!!!
+parameters = {'rollingMeanWindow': 15, 'rollingMinMaxScalerWindow': 270, 'rollingMedianWindow': 15, 'minProminenceForPeakDetect': 0.075, 'windowForLocalPeakMinMaxFind': 5, 'plotGraph': True, 'allBodyRegionsArmIncluded': False, 'plotZoomedGraph': False, 'minMaxTimeToleranceMinus': 0, 'minMaxTimeTolerancePlus': 0, 'plotGraphStrainDuringDescendingPain': False, 'zoomedGraphNbDaysMarginLeft': 14, 'zoomedGraphNbDaysMarginRight': 14}
+# parameters = {'rollingMeanWindow': 15, 'rollingMinMaxScalerWindow': 270, 'rollingMedianWindow': 15, 'minProminenceForPeakDetect': 0.03, 'windowForLocalPeakMinMaxFind': 3, 'plotGraph': True, 'allBodyRegionsArmIncluded': False, 'plotZoomedGraph': False, 'minMaxTimeToleranceMinus': 0, 'minMaxTimeTolerancePlus': 0, 'plotGraphStrainDuringDescendingPain': False}
+
+plotGraphs = True
+saveData   = True
+
+[score, totDaysTakenIntoAccount, regScore, nbAscendingDays, nbDescendingDays, nbPainPeaks] = peaksAnalysis_launch.calculateForAllRegions(data, parameters, plotGraphs, saveData)
+print("parameters:", parameters)
+print("totDaysTakenIntoAccount:", totDaysTakenIntoAccount)
+print("regScore:", regScore)
+print("nbAscendingDays: ", nbAscendingDays, "nbDescendingDays: ", nbDescendingDays)
+print("nbPainPeaks:", nbPainPeaks)
+print("")
+print("nbAscendingDays: ", nbAscendingDays + nbPainPeaks * (parameters['minMaxTimeToleranceMinus'] + parameters['minMaxTimeTolerancePlus']), "nbDescendingDays: ", nbDescendingDays - nbPainPeaks * (parameters['minMaxTimeToleranceMinus'] + parameters['minMaxTimeTolerancePlus']))
+
